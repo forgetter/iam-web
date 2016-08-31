5465b825fa07405f6a63d42bc0eb3d61d026fc3b
fogray <forget871205@126.com>
2016-08-31 16:09:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-echo '>>> Get old container id'
+echo '>>> Get old container id' > iam-build.log
 
 CID=$(sudo docker ps | grep "iam" | awk '{print $1}')
-echo $CID
-
-sudo touch iam-build.log
+echo $CID >> iam-build.log
 
 sudo docker build -t iam . | tee iam-build.log
  
 RESULT=$(cat iam-build.log | tail -n 1)
-if ["$RESULT" != *Successfully*];then
-  exit -1
-fi
+#if ["$RESULT" != *Successfully*];then
+#  exit -1
+#fi
  
 if [ "$CID" != "" ];then
-  echo '>>> Stop and remove old container'
-  sudo docker stop $CID
-  sudo docker rm $CID
+  echo '>>> Stop and remove old container' >> iam-build.log
+  sudo docker stop $CID >> iam-build.log
+  sudo docker rm $CID >> iam-build.log
 fi
 
-echo '>>> Starting new container'
-sudo docker run -d -p 10040:8080 iam
+echo '>>> Starting new container' >> iam-build.log
+sudo docker run -d -p 10040:8080 iam >> iam-build.log
